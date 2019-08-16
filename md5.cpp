238e2ee38fc77f051f6581534bd40e464924adc0
ayoub sirai <42876999+siraiayoub@users.noreply.github.com>
2019-08-16 11:49:11 -0700

    Update md5.cpp

--- md5.cpp
+++ md5.cpp
@@ -4,25 +4,6 @@
 #include <iostream>
 
 
-/*
-
-                Copyright (C) 2019  ayoub sirai
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-*/
-
 class MD5 {
     private:
         uint32_t h0, h1, h2, h3, *w, pos, a, b, c, d, f, g, temp;
